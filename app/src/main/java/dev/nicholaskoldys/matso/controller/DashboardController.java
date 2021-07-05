74b407f9ccf53fb213d5ee4a32d32378b0b6c160
Nicholas Koldys <nicholas.koldys@gmail.com>
2021-07-05 16:18:15 -0500

    Add style to pie chart, add style sheet, fix barChart size

--- app/src/main/java/dev/nicholaskoldys/matso/controller/DashboardController.java
+++ app/src/main/java/dev/nicholaskoldys/matso/controller/DashboardController.java
@@ -11,6 +11,7 @@ import javafx.collections.ObservableList;
 import javafx.event.ActionEvent;
 import javafx.fxml.FXML;
 import javafx.fxml.Initializable;
+import javafx.scene.Node;
 import javafx.scene.chart.*;
 import javafx.scene.control.*;
 import javafx.scene.control.cell.PropertyValueFactory;
